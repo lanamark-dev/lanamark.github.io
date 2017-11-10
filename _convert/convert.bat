3c732a26a7026cc829411b6698a4a50799c8250c
rinash <i.zolotarenko@gmail.com>
2017-11-10 00:04:21 -0600

    Added HPE alliance

--- _convert/convert.bat
+++ _convert/convert.bat
@@ -1,2 +1,2 @@
 @echo off
-lessc --clean-css  C:\git\_site\lanamark.github.io\lanamark.github.io\assets\styles\less\main.less  C:\git\_site\lanamark.github.io\lanamark.github.io\assets\styles\css\main.css --source-map=C:\git\_site\lanamark.github.io\lanamark.github.io\assets\styles\css\main.css.map && @echo "Convert Ok!" 
+lessc --clean-css  C:\git\_site\DEV\lanamark.github.io\assets\styles\less\main.less  C:\git\_site\DEV\lanamark.github.io\assets\styles\css\main.css --source-map=C:\git\_site\DEV\lanamark.github.io\assets\styles\css\main.css.map && @echo "Convert Ok!" 
